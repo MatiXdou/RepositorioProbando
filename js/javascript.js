4b6961a7e6b9e899a6365ee6293b158f290c7ab7
MatiXdou <163367319+MatiXdou@users.noreply.github.com>
2024-06-09 17:42:16 -0400

    mejorando validaciones from

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
+    var nombre;
+    var apellido;
     var email;
+    var motivoContacto;
+    var whyContacto;
     var formato;
     var dns;
+
+
+
+    /* Validaciones individuales */
+
+    $("#nombre").blur(function(){
+        nombre = $("#nombre").val();
+        if (nombre.length >=3 && nombre.length <= 20) {
+            $("#error1").html("");
+            console.log('nombre validado correctamente.');
+        } else {
+            $("#error1").html("<p>El nombre debe tener entre 3 y 20 caracteres.</p>");
+            return;
+        }
+    });
+
+
+    $("#apellido").blur(function(){
+        apellido = $("#apellido").val();
+        if (apellido.length >=3 && apellido.length <= 20) {
+            $("#error2").html("");
+            console.log('apellido validado correctamente.')
+        } else {
+            $("#error2").html("<p>El apellido debe tener entre 3 y 20 caracteres.</p>");
+            return;
+        }
+    });
+
+
     $("#email").blur(function(){
         email = $("#email").val();
         if(email.length > 0){
@@ -15,32 +48,82 @@ $(document).ready(function() {
                             console.log('email validado correctamente.')
                         } else {
                             $("#error3").html('<p>El email no tiene un dominio valido.</p>');
-                            $("#email").focus();
                             return;
                         }
                     } else {
                         $("#error3").html('<p>El email no tiene un formato valido.</p>');
-                            $("#email").focus();
                             return;
                     }
                     
                 });
         } else{
             $("#error3").html('<p>Debe rellenar este campo.</p>');
-            $("#email").focus();
             return;
         }
         
 
     })
 
+
+    $("#motivo-contacto").click(function(){
+        motivoContacto = $("#motivo-contacto").val();
+        if (motivoContacto!="defoption") {
+            $("#error4").html("");
+            console.log('Motivo validado correctamente.')
+        } else {
+            $("#error4").html("<p>Debe seleccionar un motivo.</p>");
+            return;
+        }
+    });
+
+
+    $("#why-contacto").blur(function(){
+        whyContacto = $("#why-contacto").val();
+        if (whyContacto.length >=10 && whyContacto.length <= 200) {
+            $("#error5").html("");
+            console.log('Mensaje contacto validado correctamente.')
+        } else {
+            $("#error5").html("<p>El mensaje debe contener entre 10 y 200 caracteres.</p>");
+            return;
+        }
+    });
+
+
+    $("#edad").blur(function(){
+        edad = $("#edad").val();
+        if (edad >= 18) {
+            $("#error2").html("");
+            console.log('Edad validada correctamente.')
+        } else {
+            $("#error2").html("<p>La edad debe ser mayor o igual a 18.</p>");
+            return;
+        }
+    });
+
+
+    $("#genero").click(function(){
+        genero = $("#genero").val();
+        if (genero!="defoption") {
+            $("#error4").html("");
+            console.log('Genero validado correctamente.')
+        } else {
+            $("#error4").html("<p>Debe seleccionar un genero.</p>");
+            $("#genero").focus();
+            return;
+        }
+    });
+
+
+
+    /* Validaciones al enviar formulario */
+
     $("#formulario-contacto").submit(function(event) {
         /*declarar variables*/
-        var nombre = $("#nombre").val();
-        var apellido = $("#apellido").val();
+        nombre = $("#nombre").val();
+        apellido = $("#apellido").val();
         email = $("#email").val();
-        var motivoContacto = $("#motivo-contacto").val();
-        var whyContacto = $("#why-contacto").val();
+        motivoContacto = $("#motivo-contacto").val();
+        whyContacto = $("#why-contacto").val();
 
         /*Validaciones*/
         if (nombre.length >=3 && nombre.length <= 20) {
